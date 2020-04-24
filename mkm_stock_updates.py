00605c7774b6e8456df6b42edd1d442c19dcbec4
dwmanuel <wouter@web-iq.nl>
2020-04-24 20:32:51 +0200

    Clean up

--- mkm_stock_updates.py
+++ mkm_stock_updates.py
@@ -1,8 +1,8 @@
-from AccountAPI import AccountAPI
+from api_classes.AccountAPI import AccountAPI
 
 
 def main():
-    aapi = AccountAPI("token_config.yml")
+    aapi = AccountAPI("configs/token_config.yml")
     stock = aapi.get_entire_stock()['article']
     aapi.update_stock_prices(stock)
 
